1c7fec87654fa4d64240045b2f36e4f8ca94b351
henrylee2cn <henrylee2cn@gmail.com>
2022-03-29 14:18:40 +0800

    feat: add methods: IsOkAnd, IsErrAnd, MapOr

--- result.go
+++ result.go
@@ -4,7 +4,7 @@ import (
 	"fmt"
 )
 
-// Result is a type that represents either success (Ok) or failure (Err).
+// Result is a type that represents either success (T) or failure (error).
 type Result[T any] struct {
 	ok  T
 	err error
@@ -25,22 +25,46 @@ func Err[T any](err any) Result[T] {
 	return Result[T]{err: newAnyError(err)}
 }
 
+// IsOk returns true if the result is Ok.
 func (r Result[T]) IsOk() bool {
 	return !r.IsErr()
 }
 
-func (r Result[T]) Ok() T {
-	return r.ok
+// IsOkAnd returns true if the result is Ok and the value inside of it matches a predicate.
+func (r Result[T]) IsOkAnd(f func(T) bool) bool {
+	if r.IsOk() {
+		return f(r.ok)
+	}
+	return false
 }
 
+// IsErr returns true if the result is error.
 func (r Result[T]) IsErr() bool {
 	return r.err != nil
 }
 
+// IsErrAnd returns true if the result is Err and the value inside of it matches a predicate.
+func (r Result[T]) IsErrAnd(f func(error) bool) bool {
+	if r.IsErr() {
+		return f(r.err)
+	}
+	return false
+}
+
+// Ok returns T, and returns empty if it is an error.
+func (r Result[T]) Ok() *T {
+	if r.IsOk() {
+		return &r.ok
+	}
+	return nil
+}
+
+// Err returns error.
 func (r Result[T]) Err() error {
 	return r.err
 }
 
+// ErrVal returns error inner value.
 func (r Result[T]) ErrVal() any {
 	if r.IsErr() {
 		return nil
@@ -48,7 +72,25 @@ func (r Result[T]) ErrVal() any {
 	if ev, _ := r.err.(*errorWithVal); ev != nil {
 		return ev.val
 	}
-	return r.err.Error()
+	return r.err
+}
+
+// Map maps a Result[T] to Result[U] by applying a function to a contained Ok value, leaving an Err value untouched.
+// This function can be used to compose the results of two functions.
+func Map[T any, U any](r Result[T], op func(T) U) Result[U] {
+	if r.IsOk() {
+		return Ok[U](op(r.ok))
+	}
+	return Err[U](r.err)
+}
+
+// MapOr returns the provided default (if Err), or applies a function to the contained value (if Ok),
+// Arguments passed to map_or are eagerly evaluated; if you are passing the result of a function call, it is recommended to use map_or_else, which is lazily evaluated.
+func MapOr[T any, U any](r Result[T], defaultOk U, op func(T) U) U {
+	if r.IsOk() {
+		return op(r.ok)
+	}
+	return defaultOk
 }
 
 func (r Result[T]) String() string {
@@ -56,11 +98,4 @@ func (r Result[T]) String() string {
 		return fmt.Sprintf("Err(%s)", r.err.Error())
 	}
 	return fmt.Sprintf("Ok(%v)", r.ok)
-}
-
-func Map[T any, B any](r Result[T], op func(T) B) Result[B] {
-	if r.IsOk() {
-		return Ok[B](op(r.ok))
-	}
-	return Err[B](r.err)
 }
